ab7810da40b0472e0125a09f4d654a27b9ccc71a
LiTao <1287862489@qq.com>
2020-09-07 17:25:53 +0800

    更新文档

--- Singleton/index.php
+++ Singleton/index.php
@@ -7,9 +7,5 @@ $db1->name = 'demo';
 $db2 =  Singleton\CommonDb::getInstance();
 $db2->name = 'demo2';
 
-/**
- * D:\Program Files (x86)\Ampps\www\github\design-patterns-php\Singleton\index.php:10:string 'demo2' (length=5)
- * D:\Program Files (x86)\Ampps\www\github\design-patterns-php\Singleton\index.php:11:string 'demo2' (length=5)
- */
 var_dump($db1->name);
 var_dump($db2->name);
